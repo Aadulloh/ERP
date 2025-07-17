143700c0901da4d5a667565881f73ddaf5a443ca
Asadulloh <asadulloh857@gmail.com>
2025-07-17 10:27:13 +0500

    fourth commit

--- src/pages/admin-layout/admin.tsx
+++ src/pages/admin-layout/admin.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Outlet, Link, useNavigate } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
 import {
   BookOutlined,
   ForkOutlined,
